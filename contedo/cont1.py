2b9e9dcdd5c46793ba9b24b89f9564ae2e8fa5d6
gabrielhemples-maker <gabriel.hemples@escola.pr.gov.br>
2026-03-05 11:42:47 -0300

    Create cont1.py

---
+++ contedo/cont1.py
@@ -0,0 +1,4 @@
+#int: para números inteiros. Ex: 7.0.-4
+#float: para números reais ou com virgúla. Ex:0.45.7.6
+#boll: para valores lógicos de verdadeiro ou falso. Ex:true ou false(sempre usar com letra maiúcula)
+#str: para strings que são palavras caracteres ou textos. Ex:'ola'.'7.5'
