5e76bd57eaec679cfcf3e82c7b7b659958a6b51c
gabrielhemples-maker <gabriel.hemples@escola.pr.gov.br>
2026-03-09 08:30:41 -0300

    Create ex004.py

---
+++ Exercicios/ex004.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite um número"))
+n2 = int(input("digite outro número"))
+
+soma= n1 + n2 
+
+#print("a soma entre {} ".format(n1),"é {}".format(n2),"é {}".format(soma))
+print("a soma entre {} e {} e {}".format(n1,n2,soma))
