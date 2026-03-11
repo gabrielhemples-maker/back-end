57ba61cc3bd9b5d70d4187c4937121432a536cac
gabrielhemples-maker <gabriel.hemples@escola.pr.gov.br>
2026-03-11 11:40:33 -0300

    Create cont4.py

---
+++ contedo/cont4.py
@@ -0,0 +1,7 @@
+#o + é utiliza como operador de adição 
+#o - utilizado como sinal de subtração 
+#o * é ultilizado para fazer a multiplicação
+# / é ultilizada para fazer divisão 
+# ** é ultilizado para fazer a exponenciação 
+# // é ultilizado para fazer a divisão inteira 
+# % é ultilizado para o resto da divisão
