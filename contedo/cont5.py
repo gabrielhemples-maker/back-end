14bbe9967a6021cde03d3c7cf8e59dd71be0c7a5
gabrielhemples-maker <gabriel.hemples@escola.pr.gov.br>
2026-03-16 11:02:04 -0300

    Create cont5.py

---
+++ contedo/cont5.py
@@ -0,0 +1,15 @@
+# Defenição: é a ordem na qual os operadores serão executados 
+# 1 Expressão dentro dos parentes 
+# 2 Em seguida vem as ponteciações 
+# 3 Em terceiro lugar vem as multações, a divisão normal, a divisão inteira e o resto da divisão. No caso de ter mais de um deles você resolve quem aparecer primeiro 
+# 4 E por ultimo a soma e a subtração 
+
+# 5 + 3 * 2 ==
+# 3 * 5 + 4 **2 == 
+# 3 * (5+4) ** 2 ==
+# O Python é utilizado em cálculos científicos é o tamanho da memoria ou seja conseguimos chegar a números extremamente grandes
+
+# Para exponenciação podemos usar o pow(x.y) onde o número x é elevado aou número y
+# Para raiz quadrada podemos usar o **(½). elevando o número a ½ nos obtemos a raiz quadrada, para raiz cúbica **(1/3). e assim por diante.
+# podemos usar os operadores em strings
+# Ex '='*5 vai ficar '====='
