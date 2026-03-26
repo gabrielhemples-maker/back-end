5e6ab9c9222723b4769dd4f55f24d56adf18f02d
gabrielhemples-maker <gabriel.hemples@escola.pr.gov.br>
2026-03-26 08:51:46 -0300

    Update cont7.py

--- contedo/cont7.py
+++ contedo/cont7.py
@@ -3,3 +3,9 @@ num = int(input('Digite um número: ' ))
 raiz = math.sqrt(num)
 print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz))) # ceil arredonda pra cima
 print('a raiz de {} é igual a {}'.format(num, math.floor(raiz)))#floor arrendonda pra baixo
+
+from math import sqrt, floor, ceil
+num = int(input('Digite um número: ' ))
+raiz = sqrt(num)
+print('a raiz de {} é igual a {}'.format(num, ceil(raiz))) #arredonda pra cima
+print('a raiz de {} é igual a {}'.format(num, floor(raiz)))
