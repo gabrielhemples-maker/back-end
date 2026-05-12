e4f360093bd83a68a9de46c6c29fec7e4db4aa1f
gabrielhemples-maker <gabriel.hemples@escola.pr.gov.br>
2026-05-12 09:43:17 -0300

    Create ex028.py

---
+++ Exercicios/ex028.py
@@ -0,0 +1,14 @@
+from random import randint
+from time import sleep #faz ele esperar
+
+aleatorio = randint(0,5)
+print("Vou pensar em um número entre 0 e 5. Tente advinhar...")
+jogador = int(input("Em que número eu pensei "))
+print("Processando...")
+
+sleep(3) #espera 3 segundos
+
+if aleatorio == jogador:
+    print("Parabens! Você conseguiu")
+else:
+    print('genhe eu pensei n numero {} e não o {}'.format(aleatorio,jogador))
